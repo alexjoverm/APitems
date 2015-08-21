68501db4706a958f50886003ae2592d024efc300
Maria <1b2marilu@gmail.com>
2015-08-21 13:37:57 +0200

    no message

--- ctrl.js
+++ ctrl.js
@@ -1,34 +1,55 @@
 angular.module('myApp').controller('Ctrl', function ($scope, $routeParams, $window) {
-//graficas  
-  $scope.labels = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10", "item11", "item12", "item13", "item14", "item15", "item16"];
-  $scope.data1 = [[65, 59, 80, 90, 100]];
-  $scope.data2 = [[80, 10, 5, 30, 20]];
-  $scope.onClick = function (points, evt) {//funcion que saca los porcentajes al ponerte encima de la barra
-    console.log(points, evt);
-  };
 
 //pestañas
+    
   $scope.tabs = [
     { title:'WinRate', content:'chart1.html' },
     { title:'PickRate', content:'chart2.html'}
   ];
+    
   //botones
+    
   $scope.radioModel = 'Normal';
   $scope.radioModel2 = 'Bronze';
-  var buyerData = {
-	labels : ["January","February","March","April","May","June"],
-	datasets : [
-		{
-			fillColor : "rgba(172,194,132,0.4)",
-			strokeColor : "#ACC26D",
-			pointColor : "#fff",
-			pointStrokeColor : "#9DB86D",
-			data : [203,156,99,251,305,247]
-		}
-	]
-  }
-  var buyers = document.getElementById('buyers').getContext('2d');
-  new Chart(buyers).Line(buyerData);
+    
+//Graficas
+    
+    var winrateData = {
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }
+        ]
+    };
+    
+    var pickrateData = {
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+        datasets: [
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+    
+    var options = {
+        
+    };
 
+  
+  angular.element(document).ready(function() {
+      var winrateChart = new Chart(document.getElementById('winrate-chart').getContext("2d")).Bar(winrateData,options);
+      var pickrateChart = new Chart(document.getElementById('pickrate-chart').getContext("2d")).Bar(pickrateData,options);
+  });
 });
 
