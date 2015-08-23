d5227461b81ed532c849aaa2dcec12f22b5a7e77
njtnestor <jodar.infmtic@gmail.com>
2015-08-23 10:31:03 +0200

    [visual] nuevos botones y tema

--- ctrl.js
+++ ctrl.js
@@ -2,18 +2,21 @@ angular.module('myApp').controller('Ctrl', function ($scope, $timeout) {
 
   //botones
 
-  $scope.radioModel = 'Normal';
-  $scope.radioModel2 = 'Bronze';
+    $scope.radioModel = 'Normal';
+    $scope.radioModel2 = 'Bronze';
+    $scope.radioModel3 = '5.11';
 
 //Graficas
     $scope.winrateData = {
         labels: ["1", "2", "3", "4", "5", "6", "7"],
-        data: [[5, 59, 80, 81, 56, 55, 40]]
+        data: [[5, 59, 80, 81, 56, 55, 40]],
+
     };
 
     $scope.pickrateData = {
         labels: ["1", "2", "3", "4", "5", "6", "7"],
-        data: [[28, 48, 40, 19, 86, 27, 90]]
+        data: [[28, 48, 40, 19, 86, 27, 90]],
+        
     };
 
     //pestañas
@@ -24,7 +27,6 @@ angular.module('myApp').controller('Ctrl', function ($scope, $timeout) {
     ];
 
     $timeout(function(){
-
         $scope.winrateData.data = [[50, 19, 111, 81, 6, 5, 40]]
     }, 2000);
 });
